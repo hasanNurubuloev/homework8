1b3a27e4e1d1cb27ec9ae66dff91d33ecbb129e1
hasanNurubuloev <hasan.nurubuloev59@gmail.com>
2019-09-15 22:43:43 +0600

    переделал тора

--- src/com/company/players/Thor.java
+++ src/com/company/players/Thor.java
@@ -5,7 +5,7 @@ import java.util.Random;
 //Добавить еще игрока, Thor, имеет шанс оглушить босса на раунд, вследствие чего босс пропускает раунд и не бьёт
 public class Thor extends Hero {
     public Thor(int health, int damage, Ability ability) {
-        super(health, damage, Ability.STUNNED );
+        super(health, damage, Ability.STUNNED);
     }
 
     public Thor(int health, int damage) {
@@ -15,13 +15,15 @@ public class Thor extends Hero {
 
     @Override
     public void useAbility(Hero[] heroes, Boss boss) {
-        Random r = new Random (1) ;
-        int a = r.nextInt(2) ;
-        if (a == 1) {
-            System.out.println("Thor stunned the boss");
-            boss.setDamage(0);
+        Random r = new Random(1);
+        int a = r.nextInt(2);
 
+        if (a == 1) {
+            boss.setDamage(0);
+            System.out.println("Thor stunned the boss");
+            boss.setDamage(30);
         }
 
     }
 }
+
